788dd96c1f836ac8f7817686eb5595365e12d826
Mikhail <centst@yandex.ru>
2020-11-10 22:44:37 +0400

    Выполнил задачу 3 (importXml($a))

--- 2.php
+++ 2.php
@@ -37,4 +37,140 @@ function mySortForKey ($a, $b) {
     return $result;
 }
 
+//----------------------------------------- Задача 3
+
+function importXml($a) {
+    $con = new mysqli("localhost","admin","", "test_samson");
+    if ($con -> connect_error) {
+        die("Connection failed: " . $con -> connect_error);
+    }
+    echo "Connection successfully";
+
+    $xmlFile = simplexml_load_file("$a");
+    $arrProducts = [];
+    $i = 0;
+    foreach ($xmlFile -> Товар as $product) {
+        $arrProducts[$i]['code'] = intval($product['Код']);
+        $arrProducts[$i]['name'] = htmlentities($product['Название']);
+        $arrPriceType = [];
+        $arrPrice = [];
+        foreach ($product -> Цена as $price) {
+            $j = 0;
+            $arrPriceType[] = htmlentities($price['Тип'][$j]);
+            $arrPrice[] = htmlentities($price[$j]);
+            $arrProducts[$i]['priceType'] = $arrPriceType;
+            $arrProducts[$i]['price'] = $arrPrice;
+            $j++;
+        }
+        unset($j);
+        foreach ($product -> Свойства as $properties) {
+            $arrProperty = [];
+            foreach ($properties as $property) {
+                $j = 0;
+                $arrProperty[] = htmlentities($property[$j]);
+                $arrProducts[$i]['property'] = $arrProperty;
+                $j++;
+            }
+            unset($j);
+        }
+        foreach ($product -> Разделы as $categories) {
+            $arrCategory = [];
+            foreach ($categories as $category) {
+                $j = 0;
+                $arrCategory[] = htmlentities($category[$j]);
+                $arrProducts[$i]['category'] = $arrCategory;
+                $j++;
+            }
+            unset($j);
+        }
+        $i++;
+    }
+    unset($i);
+
+    // Импорт в БД.
+    $count = count($arrProducts);
+    for ($j = 0; $j < $count; $j++) {
+
+        // Импорт в таблицу a_product
+        $code = $arrProducts[$j]['code'];
+        $name = $arrProducts[$j]['name'];
+        $result = mysqli_query($con, "INSERT INTO a_product (`code`, `name`) VALUES ('$code', '$name')");
+        if (!$result) {
+            $error = mysqli_error($con);
+            print("Ошибка : " . $error);
+        }
+        else {
+            $idProduct = mysqli_insert_id($con);
+            $arrProducts[$j]['product_id'] = $idProduct;
+        }
+
+        // Импорт в таблицу a_price
+        $productId = $arrProducts[$j]['product_id'];
+        $priceCount = count($arrProducts[$j]['price']);
+        for ($i = 0; $i < $priceCount; $i++) {
+            $priceType = $arrProducts[$j]['priceType'][$i];
+            $price = $arrProducts[$j]['price'][$i];
+            $result = mysqli_query($con, "INSERT INTO a_price (`product_id`, `price_type`, `price`) VALUES ('$productId', '$priceType', '$price')");
+            if (!$result) {
+                $error = mysqli_error($con);
+                print("Ошибка : " . $error);
+            }
+        }
+        unset($i);
+
+        // Импорт в таблицу a_property
+        $propertyCount = count($arrProducts[$j]['property']);
+        for ($i = 0; $i < $propertyCount; $i++) {
+            $propertyValue = $arrProducts[$j]['property'][$i];
+            $result = mysqli_query($con, "INSERT INTO a_property (`product_id`, `property_value`) VALUES ('$productId', '$propertyValue')");
+            if (!$result) {
+                $error = mysqli_error($con);
+                print("Ошибка : " . $error);
+            }
+        }
+        unset($i);
+
+        // Импорт в таблицу a_category
+        $categoryCount = count($arrProducts[$j]['category']);
+        $category = [];
+        for ($i = 0; $i < $categoryCount; $i++) {
+            $categoryName = $arrProducts[$j]['category'][$i];
+            $sql = mysqli_query($con, "SELECT `name` FROM `a_category`");
+            while ( $rec = mysqli_fetch_assoc($sql)) {
+                $category[] = $rec['name'];
+            }
+            if (! in_array($categoryName, $category)) {
+                $result = mysqli_query($con, "INSERT INTO a_category (`name`) VALUES ('$categoryName')");
+                if (!$result) {
+                    $error = mysqli_error($con);
+                    print("Ошибка : " . $error);
+                }
+            }
+        }
+        unset($i);
+
+        $categoryArr = [];
+        $idArr = [];
+        $sql = mysqli_query($con, "SELECT `id`,`name` FROM `a_category`");
+        while ( $rec = mysqli_fetch_assoc($sql)) {
+            $categoryArr[] = $rec['name'];
+            $idArr[]= $rec['id'];
+        }
+        $arr = array_combine($idArr, $categoryArr);
+
+        // Импорт в таблицу product_category
+        foreach ($arrProducts[$j]['category'] as $cat) {
+            if (in_array($cat, $arr)) {
+                $categoryId = array_search($cat, $arr);
+                $result = mysqli_query($con, "INSERT INTO product_category VALUES ('$productId', '$categoryId')");
+                if (!$result) {
+                    $error = mysqli_error($con);
+                    print("Ошибка : " . $error);
+                }
+            }
+        }
+    }
+    mysqli_close($con);
+}
+
 ?>

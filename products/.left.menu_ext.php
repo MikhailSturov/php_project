d87a1eb63999ec37d7611d82f76e224a763523de
Mikhail <centst@yandex.ru>
2021-01-25 22:19:46 +0400

    Задание 11

--- products/.left.menu_ext.php
+++ products/.left.menu_ext.php
@@ -6,8 +6,8 @@ global $APPLICATION;
 $aMenuLinksExt=$APPLICATION->IncludeComponent("bitrix:menu.sections", "", array(
 	"IS_SEF" => "Y",
 	"SEF_BASE_URL" => "",
-	"SECTION_PAGE_URL" => "#SECTION_CODE_PATH#",
-	"DETAIL_PAGE_URL" => "#SECTION_CODE_PATH#/#ELEMENT_CODE#",
+	"SECTION_PAGE_URL" => "#IBLOCK_TYPE_ID#/#SECTION_CODE_PATH#",
+	"DETAIL_PAGE_URL" => "#IBLOCK_TYPE_ID#/#SECTION_CODE_PATH#/#ELEMENT_CODE#",
 	"IBLOCK_TYPE" => "products",
 	"IBLOCK_ID" => "2",
 	"DEPTH_LEVEL" => "1",
